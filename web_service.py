755afa62c69a1070e9fc7797489d3d5bed321806
Jiang XD <jiangxiangdong@kemov.com>
2017-11-17 17:20:44 +0800

    add logic for user and task
    Signed-off-by: Jiang XD <jiangxiangdong@kemov.com>

--- web_service.py
+++ web_service.py
@@ -18,9 +18,21 @@ tasks = [
     }
 ]
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/appname/module/rest/task', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({"modid":"123","name":"abc","status":"ok","remark":"good"})
+
+@app.route('/appname/module/rest/user', methods=['GET'])
+def get_user():
+    return jsonify({'user': 'test1', 'pwd':'test'})
+
+@app.route('/appname/module/rest/task/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = filter(lambda t: t['id'] == task_id, tasks)
+    if len(task) == 0:
+        return jsonify({"modid":"1234","name":"abcd","status":"alarm","remark":"good"})
+    return jsonify({'task': task[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True,host="10.56.56.236",port=65500)
