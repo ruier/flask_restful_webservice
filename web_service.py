5bcc9e0820f720a121fa855ed2e2c0db8d4c888f
Jiang XD <jiangxiangdong@kemov.com>
2017-11-22 16:41:17 +0800

    add task list
    Signed-off-by: Jiang XD <jiangxiangdong@kemov.com>

--- web_service.py
+++ web_service.py
@@ -1,37 +1,31 @@
 #!/bin/python
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
 tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
+	{"modid":"123","name":"abc","status":"ok","remark":"good"},
+	{"modid":"111","name":"aaa","status":"ok","remark":"good"}
+	]
+json_data = [
+{"tasks":("123")},
+{"modid":"123","name":"abc","status":"ok","remark":"good"},
 ]
-
-@app.route('/appname/module/rest/task', methods=['GET'])
-def get_tasks():
-    return jsonify({"modid":"123","name":"abc","status":"ok","remark":"good"})
+@app.route('/appname/module/rest/task', methods=['GET', 'POST'])
+def tasks():
+    if request.method == 'POST':
+        return jsonify({'task': 'ok'}), 201
+    else:
+        return jsonify(json_data[0])
 
 @app.route('/appname/module/rest/user', methods=['GET'])
 def get_user():
     return jsonify({'user': 'test1', 'pwd':'test'})
 
-@app.route('/appname/module/rest/task/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = filter(lambda t: t['id'] == task_id, tasks)
-    if len(task) == 0:
-        return jsonify({"modid":"1234","name":"abcd","status":"alarm","remark":"good"})
-    return jsonify({'task': task[0]})
+@app.route('/appname/module/rest/task/1', methods=['GET'])
+def get_task():
+    return jsonify(json_data[1])
 
 
 if __name__ == '__main__':
