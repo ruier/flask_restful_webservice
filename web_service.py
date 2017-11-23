2523b338abf59bcd40afa790587d7a5a4c8bbb63
Jiang XD <jiangxiangdong@kemov.com>
2017-11-23 11:25:26 +0800

    use file to store the json data
    Signed-off-by: Jiang XD <jiangxiangdong@kemov.com>

--- web_service.py
+++ web_service.py
@@ -9,15 +9,19 @@ tasks = [
 	{"modid":"79600","name":"检查线路1","status":"未完成","remark":"2017/12/12 日截止"},
 	{"modid":"79601","name":"添加设备2","status":"完成","remark":"2017/11/11 已结束"}
 	]
-json_data = [
-{"tasks":("10001","123", "234", "79600", "79601")},
-{"modid":"10001","name":"完成flask主站","status":"完成","remark":"2017/11/23已结束"},
-{"modid":"123","name":"abc","status":"ok","remark":"good"},
-{"modid":"234","name":"bcd","status":"nook","remark":"needtocheck"},
-        {"modid":"79600","name":"检查线路1","status":"未完成","remark":"2017/12/12日截止"},
-        {"modid":"79601","name":"添加设备2","status":"完成","remark":"2017/11/11已结束"}
+#json_data = [
+#{"tasks":("10001","123", "234", "79600", "79601")},
+#{"modid":"10001","name":"完成flask主站","status":"完成","remark":"2017/11/23已结束"},
+#{"modid":"123","name":"abc","status":"ok","remark":"good"},
+#{"modid":"234","name":"bcd","status":"nook","remark":"needtocheck"},
+#        {"modid":"79600","name":"检查线路1","status":"未完成","remark":"2017/12/12日截止"},
+#        {"modid":"79601","name":"添加设备2","status":"完成","remark":"2017/11/11已结束"}
 
-]
+#]
+
+with open("./record.json",'r') as load_f:
+	json_data = json.load(load_f)
+
 @app.route('/appname/module/rest/task', methods=['GET', 'POST'])
 def tasks():
     if request.method == 'POST':
